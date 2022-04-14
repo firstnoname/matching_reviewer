a9fdc3a9fb32d990c24c1a10306af38d5452929d
iFirst <first.cmu.se@gmail.com>
2022-04-14 16:57:56 +0700

    Change index.

--- lib/views/index/index.dart
+++ lib/views/index/index.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:matching_reviewer/blocs/app_manager/bloc/app_manager_bloc.dart';
 import 'package:matching_reviewer/views/views.dart';
 
+import '../../models/models.dart';
 import '../../widgets/widgets.dart';
 
 class Index extends StatelessWidget {
@@ -45,42 +46,11 @@ class Index extends StatelessWidget {
                         MaterialPageRoute(
                           builder: (context) => const MatchingView(),
                         )),
-                    child: Card(
-                      child: Row(
-                        children: [
-                          const Padding(
-                            padding: EdgeInsets.all(16.0),
-                            child: CircleAvatar(
-                              radius: 50,
-                              child: Icon(Icons.people),
-                            ),
-                          ),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Chatnattaphon Ratnaratorn',
-                                style: Theme.of(context).textTheme.titleMedium,
-                              ),
-                              Row(
-                                children: const [
-                                  ChipGenerator(
-                                      label: 'Food', color: Color(0xFFff6666)),
-                                  ChipGenerator(
-                                      label: 'Cosmetics',
-                                      color: Color(0xFF007f5c)),
-                                  ChipGenerator(
-                                      label: 'Cloths',
-                                      color: Color(0xFF5f65d3)),
-                                  ChipGenerator(
-                                      label: 'Services',
-                                      color: Color(0xFF19ca21)),
-                                ],
-                              ),
-                            ],
-                          )
-                        ],
+                    child: UserCardWidget(
+                      userInfo: User(
+                        firstName: 'Chatnattaphon',
+                        lastName: 'Ratnaratorn',
+                        userRole: 'reviewer',
                       ),
                     ),
                   ),
